58dba0e00074bd843d8e116c7559e2366a318ded
watchmarket <94284201+watchmarket@users.noreply.github.com>
2026-01-03 18:35:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 const CONFIG_APP = {
     APP: {
         NAME: "WATCHMARKET",
-        VERSION: "12.11",
+        VERSION: "12.12",
         SCAN_LIMIT: true,
         AUTORUN: false,
         AUTO_VOLUME: false,   // Set false untuk menyembunyikan & menonaktifkan fitur autorun
